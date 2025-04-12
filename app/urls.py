8ee5a1a7212fa3f847433b3a3c07fbe7fd4c7f2d
akku patel <akky05727@gmail.com>
2025-04-12 11:27:39 +0530

    Changes in views

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -14,3 +16,6 @@ urlpatterns = [
     path('edit_profile/<int:user_id>/',views.edit_profile,name='edit_profile'),
     path('edit_payment/<int:user_id>/',views.edit_payment_info,name='edit_payment_info'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
